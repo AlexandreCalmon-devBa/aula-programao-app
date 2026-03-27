f73efefaf9954b87fa3442c212e1ae9579d95056
AlexandreCalmon-devBa <alexandre.s.junior6@ba.estudante.senai.br>
2026-03-26 21:21:55 -0300

    branch

---
+++ projeto/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Mensagem from './components/Mensagem'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="titulo-gradiente">Bem-vindo ao meu Projeto Icarus!</h1>
+      <Mensagem />
+      <Mensagem />
+      <Mensagem />
+
+    </>
+  )
+
+}
+
+export default App
---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Mensagem(){
+    return (
+        <div className='mensagem'>
+            <h1>Sparta Icarus</h1>
+            <p>Voando até você.......</p>
+        </div>
+    )
+}
